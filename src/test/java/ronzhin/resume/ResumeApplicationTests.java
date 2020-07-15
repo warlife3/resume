02eda482396f00e55e769ebbf72442d0f7fb9fb4
Mikhail Ronzhin <ronzhinm@gmail.com>
2020-07-15 18:47:42 +0300

    version 1.0

---
+++ src/test/java/ronzhin/resume/ResumeApplicationTests.java
@@ -0,0 +1,13 @@
+package ronzhin.resume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
